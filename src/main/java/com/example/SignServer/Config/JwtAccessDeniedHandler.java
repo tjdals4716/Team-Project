9ad53505f804d7fec01e422b9786adf8ffbc9469
Dong Geun <ehdrms001030@naver.com>
2023-11-06 21:59:31 +0900

    :white_check_mark: JwtAccessDeniedHandler, JwtAuthenticationEntryPoint, 이메일 중복방지 구현

--- src/main/java/com/example/SignServer/Config/JwtAccessDeniedHandler.java
+++ src/main/java/com/example/SignServer/Config/JwtAccessDeniedHandler.java
@@ -2,6 +2,7 @@ package com.example.SignServer.Config;
 
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 //  User 권한을 가진 사람이 Admin 권한이 있는 곳에 접근하려 할 때 에러메시지를 응답하기 위해
+@Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
--- src/main/java/com/example/SignServer/Config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/SignServer/Config/JwtAuthenticationEntryPoint.java
@@ -2,6 +2,7 @@ package com.example.SignServer.Config;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 // 잘못된 사용자 정보 혹은 토큰이 유효하지 않은 경우 에러메시지를 응답하는 클래스
+@Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
